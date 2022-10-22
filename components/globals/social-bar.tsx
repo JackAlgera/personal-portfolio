042b2f5dba53378e6d9c2f8bcea1c785df831575
Jacobus Algera <jacobus.algera@blablacar.com>
2022-10-22 17:51:53 +0200

    Add empty socials bar component

---
+++ components/globals/social-bar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React, {useState} from "react";
+import {useRouter} from "next/router";
+import {Watcher} from "./watcher";
+
+export const SocialBar = () => {
+  return (
+    <div className="nav">
+      <div className="nav-pos">
+      </div>
+    </div>
+  );
+};
