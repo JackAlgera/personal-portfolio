5dd461c5d755cf42576fb06e9e199a521cc24be7
JackAlgera <jack.algera@live.com>
2022-10-29 21:49:29 +0200

    refactor(title): move application title to _appi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,21 @@ import {AppProps} from "next/app";
 import {Navbar} from "../components/globals/navbar";
 import {useState} from "react";
 import {SocialBar} from "../components/globals/social-bar";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [watcherActivated, setWatcherActivated] = useState(false);
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>Jack&apos;s portfolio</title>
+      </Head>
       <Navbar watcherActivated={watcherActivated} setWatcherActivated={setWatcherActivated} />
       <div className="context">
         <Component {...pageProps} watcherActivated={watcherActivated} setWatcherActivated={setWatcherActivated} />
       </div>
       <SocialBar watcherActivated={watcherActivated} setWatcherActivated={setWatcherActivated} />
-    </div>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Jack&apos;s portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet" />
