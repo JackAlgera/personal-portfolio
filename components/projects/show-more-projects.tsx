14d3fa50dd551a7aeb23746336b37830b8b4435f
JackAlgera <jack.algera@live.com>
2022-11-02 23:00:57 +0100

    refactor(show-more-projects): refactor showMoreProjects component slightly

--- components/projects/show-more-projects.tsx
+++ components/projects/show-more-projects.tsx
@@ -3,17 +3,24 @@ import {Project, PROJECTS} from "../_models/project.model";
 import styles from "./show-more-projects.module.scss";
 import {ProjectImage} from "./project-image";
 
+const MORE_PROJECTS_SENTENCES = [
+  'Hey you, yes you, check out these other projects !',
+  'Projects, projects and more projects !'
+];
+
 export const ShowMoreProjects = () => {
+  const [sentence, setSentence] = useState('');
   const [projects, setProjects] = useState<Project[]>([]);
 
   useEffect(() => {
     let randomProjects = Array.from(PROJECTS.values()).sort(() => 0.5 - Math.random()).slice(0, 2);
     setProjects(randomProjects);
+    setSentence(MORE_PROJECTS_SENTENCES[Math.floor(Math.random() * MORE_PROJECTS_SENTENCES.length)]);
   }, []);
 
   return (
     <div className={styles.container}>
-      <h2>Psst, check out these other gucci projects</h2>
+      <h2>{sentence}</h2>
       <div className={styles.imagesContainer}>
         {projects.map(project => (
           <div key={project.title}>
