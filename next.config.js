16a355ed457ec35baacde4a9037396cc1550e405
JackAlgera <jack.algera@live.com>
2022-12-09 17:44:55 +0100

    fix(svg): fix svg imports with next/image usage

--- next.config.js
+++ next.config.js
@@ -3,30 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+        (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.icon\.svg$/;
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [{
-        loader: '@svgr/webpack',
-        options: {
-          svgoConfig: {
-            plugins: [
-              {
-                name: 'preset-default',
-                params: {
-                  overrides: {
-                    cleanupIDs: false,
-                    collapseGroups: false
-                  }
+      test: /\.icon\.svg$/,
+      loader: '@svgr/webpack',
+      options: {
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  cleanupIDs: false,
+                  collapseGroups: false
                 }
               }
-            ]
-          }
+            }
+          ]
         }
-      }],
-    })
-
-    return config
+      }
+    });
+    return config;
   },
 }
 
